cbb41781cdd3530165ba5ed6fda8fde1ef4e67d3
Belka-stack <Belkabakouche@gmail.com>
2026-08-17 09:53:17 +0200

    Ajout des /auth/me au service profil

--- src/services/profilService.js
+++ src/services/profilService.js
@@ -2,12 +2,12 @@ import api from './api'; // Import de l'instance Axios centralisée
 
 // Récupérer les données du profil de l'utilisateur connecté
 export const getMyProfile = async () => {
-    const response = await api.get('/profile/me/');
+    const response = await api.get('/auth/me');
     return response.data;
 };
 
 // Mettre à jour les données du profil (PATCH)
 export const updateMyProfile = async (profileData) => {
-    const response = await api.patch('/profile/me/', profileData);
+    const response = await api.patch('/auth/me', profileData);
     return response.data;
 };
